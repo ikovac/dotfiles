af24a2aa54bddd1a654f46f0fced29de285a9ed1
Ivo Kovacevic <ikovacevic96k@gmail.com>
2022-11-06 21:15:09 +0100

    Disable nord bold

--- nvim/.config/nvim/after/plugin/colorscheme.lua
+++ nvim/.config/nvim/after/plugin/colorscheme.lua
@@ -1,6 +1,7 @@
 vim.g.nord_italic = false
 vim.g.nord_contrast = false
 vim.g.nord_borders = true
+vim.g.nord_bold = false
 
 local status, _ = pcall(vim.cmd, "colorscheme nord")
 if not status then
