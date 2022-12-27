d94da486afbe9c6ad813a77cd0029d4883c1268d
Ivo Kovacevic <ikovacevic96k@gmail.com>
2022-12-27 15:01:07 +0100

    Use nord set to set nord theme

--- nvim/.config/nvim/after/plugin/colorscheme.lua
+++ nvim/.config/nvim/after/plugin/colorscheme.lua
@@ -1,10 +1,11 @@
+local nord_status, nord = pcall(require, "nord")
+if not nord_status then
+	return
+end
+
 vim.g.nord_italic = false
 vim.g.nord_contrast = false
 vim.g.nord_borders = true
 vim.g.nord_bold = false
 
-local status, _ = pcall(vim.cmd, "colorscheme nord")
-if not status then
-	print("Colorscheme not found!")
-	return
-end
+nord.set()
